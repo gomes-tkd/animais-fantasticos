d7a1ddb8db2138185af2b504021d426945a4207a
Jose Gomes <jgomestkd@gmail.com>
2025-06-11 17:49:42 -0300

    Internal Scroll smooth

--- index.js
+++ index.js
@@ -37,3 +37,27 @@ function initAccordion() {
 }
 
 initAccordion();
+
+function initScrollSmooth() {
+    const internalLinks = document.querySelectorAll(".js-menu a[href^='#']");
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute("href");
+        const section = document.querySelector(href);
+
+        section.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        });
+
+        // forma alternativa
+        // const topSection = section.offsetTop;
+        // window.scrollTo({ top: topSection, behavior: "smooth" });
+    }
+
+    internalLinks.forEach((link) => link.addEventListener("click", scrollToSection));
+}
+
+initScrollSmooth();
+
