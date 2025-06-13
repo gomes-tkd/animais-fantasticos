969f5f740ea8b4c9aa4686bd42a1e6088a24a842
Jose Gomes <jgomestkd@gmail.com>
2025-06-13 12:11:31 -0300

    Fixing Scroll Animation:

--- index.js
+++ index.js
@@ -1,92 +1,110 @@
 function initTabNavigation() {
-    const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-    const tabContent = document.querySelectorAll('[data-tab="content"] section');
+  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+  const tabContent = document.querySelectorAll('[data-tab="content"] section');
 
-    console.log(tabMenu);
-    console.log(tabContent);
-    const activeClass = "active";
+  console.log(tabMenu.length);
+  console.log(tabContent.length);
 
-    if (tabContent.length && tabMenu.length) {
-        tabContent[0].classList.add("active");
+  tabMenu.forEach((item) => {
+    console.log(item);
+  });
+  tabContent.forEach((item) => {
+    console.log(item);
+  });
 
-        function activeTab(index) {
-            tabContent.forEach((section) => {
-                section.classList.remove("active")
-            });
+  const activeClass = "active";
 
-            const directionAnimation = tabContent[index].dataset.anime;
-            tabContent[index].classList.add("active", directionAnimation);
-        }
+  if (tabContent.length && tabMenu.length) {
+    tabContent[0].classList.add(activeClass);
 
-        tabMenu.forEach((itemMenu, index) => (
-            itemMenu.addEventListener("click", () => activeTab(index))
-        ));
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove(activeClass);
+      });
+
+      const directionAnimation = tabContent[index].dataset.anime;
+      tabContent[index].classList.add(activeClass, directionAnimation);
     }
+
+    tabMenu.forEach((itemMenu, index) =>
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      })
+    );
+  }
 }
 
 initTabNavigation();
 
 function initAccordion() {
-    const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
-    const activeClass = "active";
+  const accordionList = document.querySelectorAll(
+    '[data-anime="accordion"] dt'
+  );
+  const activeClass = "active";
 
-    if (accordionList.length) {
-        accordionList[0].classList.add(activeClass);
-        accordionList[0].nextElementSibling.classList.add(activeClass);
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
 
-        function activeAccordion() {
-            this.classList.toggle(activeClass);
-            this.nextElementSibling.classList.toggle(activeClass);
-        }
-
-        accordionList.forEach((item) =>  item.addEventListener("click", activeAccordion));
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
     }
+
+    accordionList.forEach((item) =>
+      item.addEventListener("click", activeAccordion)
+    );
+  }
 }
 
 initAccordion();
 
 function initScrollSmooth() {
-    const internalLinks = document.querySelectorAll("[data-menu='suave'] a[href^='#']");
+  const internalLinks = document.querySelectorAll(
+    "[data-menu='suave'] a[href^='#']"
+  );
 
-    function scrollToSection(event) {
-        event.preventDefault();
-        const href = event.currentTarget.getAttribute("href");
-        const section = document.querySelector(href);
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
 
-        section.scrollIntoView({
-            behavior: "smooth",
-            block: "start",
-        });
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
 
-        // forma alternativa
-        // const topSection = section.offsetTop;
-        // window.scrollTo({ top: topSection, behavior: "smooth" });
-    }
+    // forma alternativa
+    // const topSection = section.offsetTop;
+    // window.scrollTo({ top: topSection, behavior: "smooth" });
+  }
 
-    internalLinks.forEach((link) => link.addEventListener("click", scrollToSection));
+  internalLinks.forEach((link) =>
+    link.addEventListener("click", scrollToSection)
+  );
 }
 
 initScrollSmooth();
 
 function initAnimationAutoScroll() {
-    const sections = document.querySelectorAll("[data-animation='scroll']");
+  const sections = document.querySelectorAll("[data-anime='scroll']");
 
-    if (sections.length) {
-        const windowHalf = (window.innerHeight * 0.6);
+  if (sections.length) {
+    const windowHalf = window.innerHeight * 0.6;
 
-        function animaScroll() {
-            sections.forEach((section) => {
-                const sectionTop = section.getBoundingClientRect().top;
-                const isSectionVisible = (sectionTop - windowHalf) < 0;
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowHalf < 0;
 
-                if (isSectionVisible) {
-                    section.classList.add("active");
-                }
-            });
+        if (isSectionVisible) {
+          section.classList.add("active");
         }
-
-        window.addEventListener("scroll", animaScroll);
+      });
     }
+
+    window.addEventListener("scroll", animaScroll);
+  }
 }
 
 initAnimationAutoScroll();
