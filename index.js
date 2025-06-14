5c1e1c923c9e7c31b8459d561aba0238c35334a0
Jose Gomes <jgomestkd@gmail.com>
2025-06-14 19:39:12 -0300

    Modal login logic

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import initScrollSmooth from "./src/scroll-smooth/scroll-smooth.js";
 import initAnimationAutoScroll from "./src/animation-auto-scroll/animation-auto-scroll.js";
 import initAccordion from "./src/accordion-list/accordion-list.js";
 import initTabNavigation from "./src/tab-navigation/tab-navigation.js";
+import initModal from "./src/modal/modal.js";
 
 initScrollSmooth();
 initAnimationAutoScroll();
 initAccordion();
 initTabNavigation();
-
+initModal();
